4ba9c1105d0064374839e2a946670b4458d1327e
rafapalacios5 <147772770+rafapalacios5@users.noreply.github.com>
2025-04-27 23:28:58 -0500

    Add files via upload

---
+++ semana 03/mergeSort.java
@@ -0,0 +1,56 @@
+
+package mergesort;
+
+public class mergeSort {
+    
+    public static void main(String[] args) {
+        int[] A = {10, 9, 8, 5, 4, 3};
+
+        System.out.println("Array original:");
+        for (int value : A) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
+        int p = 0;
+        int r = A.length - 1;
+        int q = (r / 2); // Ojo: no -1, para que sea exactamente la mitad
+
+        // Ahora hacemos la "merge" manualmente
+        int nL = q - p + 1;
+        int nR = r - q;
+
+        int[] L = new int[nL];
+        int[] R = new int[nR];
+
+        for (int i = 0; i < nL; i++) {
+            L[i] = A[p + i];
+        }
+        for (int j = 0; j < nR; j++) {
+            R[j] = A[q + 1 + j];
+        }
+
+        int i = 0, j = 0, k = p;
+        while (i < nL && j < nR) {
+            if (L[i] <= R[j]) {
+                A[k++] = L[i++];
+            } else {
+                A[k++] = R[j++];
+            }
+        }
+
+        while (i < nL) {
+            A[k++] = L[i++];
+        }
+
+        while (j < nR) {
+            A[k++] = R[j++];
+        }
+
+        System.out.println("Array parcialmente ordenado:");
+        for (int value : A) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
